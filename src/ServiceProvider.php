72f2d4d0a602ff084031a2ca44bf26773b684f20
Burak <bsormagec@gmail.com>
2018-11-06 21:50:42 +0300

    Fix

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,39 +5,26 @@ use OpenTok\OpenTok;
 
 class ServiceProvider extends BaseServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->mergeConfigFrom($this->configPath(), 'opentok');
-	}
-
+    /**
+     * Bootstrap the application services.
+     */
     public function boot()
     {
-        $this->publishes([$this->configPath() => config_path('opentok.php')], 'config');
-
-        $this->app->singleton('OpentokApi', function($app) {
-			return new OpenTok(
+        $this->publishes([
+            __DIR__ . '/../config/opentok.php' => config_path('opentok.php'),
+        ], 'config');
+    }
+	
+	/**
+     * Register the application services.
+     */
+	public function register(){
+        $this->mergeConfigFrom(__DIR__ . '/../config/opentok.php', 'laravel-ffmpeg');
+        $this->app->singleton('OpentokApi', function ($app) {
+            return new OpenTok(
 				$app['config']->get('opentok')['api_key'],
 				$app['config']->get('opentok')['api_secret']
 			);
-		});
+        });
     }
-
-	protected function configPath()
-	{   
-		return __DIR__ . '/../config/opentok.php';
-	}   
-
 }
